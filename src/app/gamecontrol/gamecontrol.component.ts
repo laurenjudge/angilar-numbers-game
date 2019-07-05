0ae309a271a440d3adcdd41e88d77bcbc2c888c7
Lauren Judge <l.m.judge@hotmail.com>
2019-07-05 14:51:53 +1200

    Gamecontrol event working

--- src/app/gamecontrol/gamecontrol.component.ts
+++ src/app/gamecontrol/gamecontrol.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-gamecontrol',
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gamecontrol.component.css']
 })
 export class GamecontrolComponent implements OnInit {
+  intervalFired = new EventEmitter<number>();
+  interval;
+  lastNumber = 0;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onStartGame() {
+    this.interval = setInterval(() => {}, 1000);
+    this.intervalFired.emit(this.lastNumber + 1);
+    this.lastNumber++;
   }
 
 }
